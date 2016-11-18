d91ecef495f6343b2fa959fedff0c27dfd6f29af
Marco Zunino UI Studio <eng.marco.zunino@gmail.com>
2016-11-18 19:11:08 +0000

    Adjusted test_delay.py test case

--- tests/test_delay.py
+++ tests/test_delay.py
@@ -8,6 +8,7 @@ import io
 import json
 import time
 import socket
+import errno
 from connector import Connector
 
 config = { \
@@ -18,18 +19,18 @@ config = { \
     "parameter" : 3, \
     "listeningPort" : 80, \
     "listeningAddress" : "testing.com", \
-    "listeningInterface" : "wlan0" \
+    "listeningInterface" : "lo" \
 }
 
 
-def connect( time = 3, server_address = ('192.168.13.1', 80) ):
+def connect( time = 3, server_address = ('127.0.0.1', 80) ):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(time)
     sock.connect(server_address)
 
 # Same as connect(), just return an error code instead of 
 # raising an exception if the connection fails
-def connect_ex( time = 3, server_address = ('192.168.13.1', 80) ):
+def connect_ex( time = 3, server_address = ('127.0.0.1', 80) ):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(time)
     return sock.connect_ex(server_address)
@@ -49,13 +50,25 @@ def runConnector():
 # TCZ will wait 3 seconds before reply to SYN.
 # connect has a timeout of 1 in this test case.
 # TEST PASS if the connect(1) raise the Timeout
-# exception 
+# exception.
+#
+# NOTE: Not clear why but here the connect_ex return 
+#       errno 11 (EAGAIN), but the exception name raised
+#       by connect() is timeout. This is not clear but
+#       I'll mark the test case to cover the timeout.
 
-def test_delay1(runConnector):
+def test_delay_1(runConnector):
     with pytest.raises(socket.timeout):
+#    try:
         connect(2)
+#    except socket.error, v:
+#        errorcode = v[0]
+#        assert errorcode == 110
+
+
+
 
 # In this case, the connection should happen 
 # correctly
-def test_delay2(runConnector):
+def test_delay_2(runConnector):
     assert connect_ex(5) == 0
