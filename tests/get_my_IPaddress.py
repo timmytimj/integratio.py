61dc38bdf7e6dd8c958106a4164019cba29b044a
raja <Rajadurai_kanniappan>
2016-11-20 09:39:02 +0100

    added a method to get ip address dynamically in tests/..

---
+++ tests/get_my_IPaddress.py
@@ -0,0 +1,14 @@
+import socket
+import fcntl
+import struct
+
+def get_my_IPaddress(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+# usage
+# get_my_IPaddress('eth0')  # '192.168.0.110'
